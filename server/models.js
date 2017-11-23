67026738f95a71cc8d369895a2f54e3a0f1025f8
Peter Andringa <peter@andrin.ga>
2017-11-23 00:29:49 -0500

    populate from ProPublica API

--- server/models.js
+++ server/models.js
@@ -1,49 +1,150 @@
 "use strict";
 const Sequelize = require("sequelize");
+const got = require('got');
+const ProPublica = require('./ProPublica.js');
+const config = require('./config.json')[process.env.NODE_ENV || "development"];
+const db = {
+  sequelize: new Sequelize(config.db_uri)
+};
 
-const config = require('config.json')[process.env.NODE_ENV || "development"];
-const db = {};
-db.sequelize = new Sequelize(config.DB_URI);
+db.Update = db.sequelize.define('updates', {
+  success: Sequelize.BOOLEAN,
+  error: Sequelize.TEXT
+}, {
+  underscored: true
+});
 
 db.Race = db.sequelize.define('race', {
   state: Sequelize.STRING,
   district: Sequelize.INTEGER, // 0 if senate, district # if house
-  total_receipts: Sequelize.INTERGER,
+  total_contrib: Sequelize.INTEGER,
   total_disbursements: Sequelize.INTEGER
 }, {
-  classMethods: {
-    associate: function(models) {
-      db.Race.hasMany(db.Candidate);
-    }
-  },
-  instanceMethods: { },
-  indexes: [ ]
+  underscored: true
 });
 
 db.Candidate = db.sequelize.define('candidate', {
+  fec_id: Sequelize.STRING,
   first_name: Sequelize.STRING,
   last_name: Sequelize.STRING,
   party: Sequelize.STRING,
-  status: Squelize.STRING,
-  individual_receipts: Sequelize.INTEGER,
-  pac_receipts: SEQUELIZE.INTEGER,
-  disbursements: SEQUELIZE.INTEGER
+  status: Sequelize.STRING,
+  individual_contrib: Sequelize.FLOAT,
+  pac_contrib: Sequelize.FLOAT,
+  total_contrib: Sequelize.FLOAT,
+  disbursements: Sequelize.FLOAT
 }, {
-  underscore: true,
-  classMethods: {
-    associate: function(models) {
-      db.Candidate.belongsTo(db.Race);
+  underscored: true,
+  getterMethods: {
+    full_name() {
+      return this.first_name + ' ' + this.last_name
     }
   },
-  instanceMethods: { },
-  indexes: [ ]
+
+  setterMethods: {
+    full_name(val) {
+      var [first, last] = val.indexOf(',') > 0 ? val.split(', ').reverse() : val.split(' ');
+      [first,last] = [first,last].map(n => n.substring(0,1).toUpperCase() + n.substring(1).toLowerCase());
+      this.setDataValue('first_name', first);
+      this.setDataValue('last_name', last);
+    },
+  }
 });
 
 // Build associations
-Object.keys(db).forEach(function(modelName) {
-  if ("associate" in db[modelName]) {
-    db[modelName].associate(db);
-  }
-});
+db.Candidate.belongsTo(db.Race);
+db.Race.hasMany(db.Candidate);
+db.sequelize.sync();
+
+// API / DB Loaders
+db.loadAll = function(){
+  return Promise.all(
+    Object.keys(require('./states.json')).map(state => this.loadState(state))
+  ).then(function(states){
+    db.Update.create({
+      success: true
+    });
+  }).catch(e => {
+    db.Update.create({
+      success: false,
+      error: e.message
+    });
+  })
+}
+
+db.loadState = function(state){
+  return got(`https://api.propublica.org/campaign-finance/v1/${config.year}/races/${state}.json`, {
+    headers: {
+      'X-API-Key': config.campaign_finance_key
+    },
+    json: true
+  }).then( res => { // Build Races
+    var races = {};
+    for(var race_data of res.body.results){
+      const [race_uri, state, race] = race_data.district.match(/\/races\/([A-Z]{2})\/([\w\/]*)\.json/);
+      const district = race === 'senate' ? 0 : parseInt(race.match(/house\/(\d*)/)[1]);
+      races[state+'-'+district] = {state: state, district: district}
+    }
+    return Promise.all([
+      res,
+      Promise.all(Object.values(races).map(race => {
+        return db.Race.findOrCreate({where: {
+          state: race.state,
+          district: race.district,
+        }})
+      }))
+    ]);
+  }).then( ([res, races]) => { // Build Candidates
+    var raceDict = {};
+    var raceIds = [];
+    for(const [race, created] of races){
+      raceDict[race.state+"-"+race.district] = race;
+      raceIds.push(race.id);
+    }
+    return Promise.all([
+      res,
+      raceDict,
+      db.Candidate.destroy({where: {race_id: raceIds}})
+    ])
+  }).then( ([res, raceDict]) => {
+    return Promise.all(
+    res.body.results.filter(d => d.candidate).map(race_data => {
+      const [race_uri, state, race] = race_data.district.match(/\/races\/([A-Z]{2})\/([\w\/]*)\.json/);
+      const district = race === 'senate' ? 0 : parseInt(race.match(/house\/(\d*)/)[1]);
+      return Promise.all([
+        got(`https://api.propublica.org/campaign-finance/v1/${config.year}${race_data.candidate.relative_uri}`, {
+          headers: {'X-API-Key': config.campaign_finance_key},
+          json: true
+        }),
+        raceDict[state+'-'+district].update({
+          total_contrib: 0,
+          total_disbursements: 0
+        })
+      ]).then( ([res, race]) => {
+        return Promise.all([
+          db.Candidate.create({
+            race_id: race.id,
+            fec_id: res.body.results[0].id,
+            full_name: res.body.results[0].name,
+            party: res.body.results[0].party.substring(0,1),
+            status: res.body.results[0].status,
+            individual_contrib: res.body.results[0].total_from_individuals,
+            pac_contrib: res.body.results[0].total_from_pacs,
+            total_contrib: res.body.results[0].total_contributions,
+            disbursements: res.body.results[0].total_disbursements,
+          }),
+          race.update({
+            total_contrib: race.total_contrib + res.body.results[0].total_contributions,
+            total_disbursements: race.total_disbursements + res.body.results[0].total_disbursements
+          })
+        ]);
+      }).catch(e => {
+        throw new Error(`Error in inner chain: ${e.stack}`);
+      }) // End Inside Chain
+    })); // End Promise.all
+  }).catch(e => {
+    throw new Error(`Error in outer chain: ${e.stack}`);
+  }); // End .then
+}
 
 module.exports = db;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 
 const ProPublica = require('./ProPublica');
-const config = require('config.json')[process.env.NODE_ENV || "development"];
+const config = require('./config.json')[process.env.NODE_ENV || "development"];
+const db = require('./models.js');
+const app = express();
 
 // If DB is empty
   // Load data from ProPublica
-function loadData() {
-  
-}
+
 
 
 // For Request:
   // Read Data from DB
   // If data > 1 day old, queue new data
-
+app.get('/', (req, res) => {
+  db.loadAll()
+    .then((results) => {
+      res.send('Done!');
+    });
+});
 
 // CRON JOB - Load Data from ProPublica once a week
+
+const port = process.env.PORT || config.port || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}...`))
